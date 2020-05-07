f8680c2080eda59fd7f94921aa59b58722db9d93
Ana Gheorghe <ana.gheorghe@scotiabank.com>
2020-05-06 22:58:54 -0400

    Moved default action to its own function

--- servo_test.py
+++ servo_test.py
@@ -16,23 +16,27 @@ door.start(7.5) #door starts closed
 arm.start(12.5) #arm starts inside the box
 alligator.start(2.5) #alligator starts facing the back
 
+def default_action:
+  if GPIO.input(10) == 1:
+    print("switched on")
+    door.ChangeDutyCycle(2.5) #open door
+    time.sleep(0.5)
+    alligator.ChangeDutyCycle(12.5) #turn alligator head to front
+    time.sleep(2)
+    arm.ChangeDutyCycle(2.5) #move arm out to close switch
+    time.sleep(0.5)
+  else:
+    print("switched off")
+    arm.ChangeDutyCycle(12.5) #move arm back in
+    time.sleep(0.5)
+    alligator.ChangeDutyCycle(2.5) #turn alligator head back
+    time.sleep(0.5)
+    door.ChangeDutyCycle(7.5) #close door
+
+
 def switch_activated(channel):
     time.sleep(0.005)
-    if GPIO.input(10) == 1:
-        print("switched on")
-        door.ChangeDutyCycle(2.5) #open door
-        time.sleep(0.5)
-        alligator.ChangeDutyCycle(12.5) #turn alligator head to front
-        time.sleep(2)
-        arm.ChangeDutyCycle(2.5) #move arm out to close switch
-        time.sleep(0.5)
-    else:
-        print("switched off")
-        arm.ChangeDutyCycle(12.5) #move arm back in
-        time.sleep(0.5)
-        alligator.ChangeDutyCycle(2.5) #turn alligator head back
-        time.sleep(0.5)
-        door.ChangeDutyCycle(7.5) #close door
+    default_action()
 
 
 GPIO.add_event_detect(10, GPIO.BOTH, callback=switch_activated, bouncetime=20)
